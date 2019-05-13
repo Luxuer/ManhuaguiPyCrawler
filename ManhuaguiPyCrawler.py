748423a89e56ef13f28ac6da841b5f365630eeee
Luxuer <470689430@qq.com>
2019-05-13 10:26:40 +0800

    download first page of the commic website

--- ManhuaguiPyCrawler.py
+++ ManhuaguiPyCrawler.py
@@ -5,7 +5,9 @@ import requests
 
 
 # 漫画柜里自杀岛的网址
-url = "https://www.manhuagui.com/comic/4375/"
+url = "https://www.manhuagui.com/comic/4375/37875.html"
+# url = "https://www.manhuagui.com/comic/4375/"
+
 # 获取url的Response
 res = requests.get(url)
 try:
@@ -13,7 +15,7 @@ try:
 except Exception as exc:
     print "There is a problem: %s" % (exc)
 
-playFile = open("index.html", "wb")
+playFile = open("001.html", "wb")
 for chunk in res.iter_content(100000):
     playFile.write(chunk)
 # print res.text
