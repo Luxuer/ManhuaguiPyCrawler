6b1997c66b4c61bf6c492612a7cc8801154d46b4
Luxuer <470689430@qq.com>
2019-05-13 12:36:20 +0800

    使用bs4美化了下载到的html

--- ManhuaguiPyCrawler.py
+++ ManhuaguiPyCrawler.py
@@ -3,11 +3,9 @@
 
 import requests
 
-
 # 漫画柜里自杀岛的网址
-url = "https://www.manhuagui.com/comic/4375/37875.html"
-# url = "https://www.manhuagui.com/comic/4375/"
-
+url = "https://www.manhuagui.com/comic/4375/37875.html" # 漫画第一页
+# url = "https://www.manhuagui.com/comic/4375/" # 下载漫画介绍页
 # 获取url的Response
 res = requests.get(url)
 try:
@@ -15,9 +13,16 @@ try:
 except Exception as exc:
     print "There is a problem: %s" % (exc)
 
-playFile = open("001.html", "wb")
-for chunk in res.iter_content(100000):
-    playFile.write(chunk)
+# 把大古熬成汤(误), 正确是把文本变成汤
+import bs4
+soup = bs4.BeautifulSoup(res.text, "lxml")
+
+# 将html写入文件
+import codecs
+playFile = codecs.open("001.html", "w", "utf-8")
+# playFile = codecs.open("index.html", "w", "utf-8")
+playFile.write(soup.prettify())
+
 # print res.text
 
 
